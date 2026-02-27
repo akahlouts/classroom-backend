acf3b2edbc27d5dbfef48faf70e54e9a75fed77c
AbdElRahman KahlOut <akahlouts@gmail.com>
2026-02-28 00:14:53 +0200

    implement fetch teachers and create classes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 
 import subjectsRouter from "./routes/subjects.js";
+import usersRouter from "./routes/users.js";
+import classesRouter from "./routes/classes.js";
 import securityMiddleware from "./middleware/security.js";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
@@ -24,12 +26,14 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
+app.use("/api/subjects", subjectsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/classes", classesRouter);
+
 app.use(securityMiddleware);
 
-app.use("/api/subjects", subjectsRouter);
-
-app.get("/", (_req, res) => {
-  res.json({ message: "Hello, Welcome to the Classroom API!" });
+app.get("/", (req, res) => {
+  res.send("Hello, welcome to the Classroom API!");
 });
 
 export default app;
